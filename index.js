0e23a9810aad2d3e2e8ce04b9bce62324fbe51b7
EducaRep <djrepolhonervoso@gmail.com>
2024-02-22 22:38:14 -0300

    Teste de classe e objeto

---
+++ index.js
@@ -0,0 +1,42 @@
+let heroi = {
+
+nome: "Eduardo",
+idade: "59 anos",
+tipo: "mago",
+ataque: "Magia.",
+
+};+
+	console.log("O " + heroi.nome + " é um " + heroi.tipo + " de " + heroi.idade + " e atacou usando " + heroi.ataque)
+
+ heroi = {
+
+nome: "Pedro",
+idade: "30 anos",
+tipo: "guereiro",
+ataque: "Espada.",
+
+};+
+	console.log("O " + heroi.nome + " é um " + heroi.tipo + " de " + heroi.idade + " e atacou usando " + heroi.ataque)
+    
+     heroi = {
+
+nome: "Orlando",
+idade: "70 anos",
+tipo: "monge",
+ataque: "Artes Marciais.",
+
+};+
+	console.log("O " + heroi.nome + " é um " + heroi.tipo + " de " + heroi.idade + " e atacou usando " + heroi.ataque)
+   
+    heroi = {
+
+nome: "Frederico",
+idade: "40 anos",
+tipo: "ninja",
+ataque: "Shuriken.",
+
+};+
+	console.log("O " + heroi.nome + " é um " + heroi.tipo + " de " + heroi.idade + " e atacou usando " + heroi.ataque)
+
+
+
